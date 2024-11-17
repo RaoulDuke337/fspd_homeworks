4f8197e8f5faa0e85c896275f30ca92b3ab7ce76
nikita.protasov <gomer12370@gmail.com>
2024-11-17 12:05:47 +0300

    modified:   files/files.py 	new file:   files/texts/1.txt 	new file:   files/texts/2.txt 	new file:   files/texts/3.txt 	new file:   files/texts/result.txt

--- files/files.py
+++ files/files.py
@@ -7,7 +7,8 @@ class Cookbook():
         self.encoding = encoding
         self.cook_book = {}
         self.ingridient_recomendation = {}
-        
+
+# Прочитываем книгу во вложенный словарь Название блюда: [Словари с ингридиентами]
     def reader(self):
         cook_book = {}
         with open(self.filepath, encoding=self.encoding) as f:
@@ -40,17 +41,49 @@ class Cookbook():
         return self.ingridient_recomendation
 
             
+def read_files(filepath, files_list):
+    texts_list = []
+    for file in os.listdir(filepath):
+        path_to_file = os.path.join(filepath, file)
+        if file in files_list:
+            with open(path_to_file) as f:
+                texts_list.append(f.readlines())
+    return texts_list
+
+def write_file_sorted(filepath, texts: list):
+    open(filepath, 'w').close()
+    sotred_texts = sorted(texts, reverse=True)
+    for idx, text in enumerate(sotred_texts):
+        name = str(idx + 1) + '.txt' + '\n'
+        rows = str(len(text)) + '\n'
+        with  open(filepath, 'a', encoding='utf-8') as file:
+            file.write(name)
+            file.write(rows)
+            file.writelines(text)
+            file.write('\n')
 
 
-
+# Задача №1. Делаем словарь поваренной книги экземпляром класса Cookbook
 filepath = os.path.join(os.getcwd(), 'files', 'Омлет.txt')
 
-# with open(filepath, encoding='utf-8') as f:
-#     for row in f:
-#         print(row)
-
+# При инициализации экземпляра не происходит чтения, но предполагается, что при вызове других функций это будет происходить
 cook_book = Cookbook(filepath=filepath, encoding='utf-8')
+
+# Задача №2 
 dishes_list = ['Запеченный картофель', 'Омлет']
 persons = 2
 
 pprint(cook_book.get_shop_list_by_dishes(dishes_list, persons))
+
+
+filepath_3 = os.path.join(os.getcwd(), 'files', 'texts')
+files = ['1.txt', '2.txt', '3.txt']
+
+# Задача №3
+# Сначала читаем файлы и кладем в пременную 
+texts_list = read_files(filepath_3, files)
+
+path_to_write = os.path.join(os.getcwd(), 'files', 'texts', 'result.txt')
+
+# Затем записываем в итоговый файл с помощью функции
+write_file_sorted(path_to_write, texts_list)
